37e88b06d68f6858c4e9e9b0182582303d08bc0a
Karol Szydlik <49267388+Szmydlo@users.noreply.github.com>
2022-05-26 21:30:16 +0200

    Headless testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,8 @@ const config: PlaywrightTestConfig = {
 		actionTimeout: 0,
 		/* Base URL to use in actions like `await page.goto('/')`. */
 		// baseURL: 'http://localhost:3000',
-		headless: false,
+		// TODO: Add logic for headed in dev and headless in prod
+		// headless: false,
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
