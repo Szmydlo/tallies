06eec60ed7b99998f9174d04c2285297b1a14e0f
Karol Szydlik <49267388+Szmydlo@users.noreply.github.com>
2022-06-10 20:31:42 +0200

    Extend overview page

--- pages/overview.tsx
+++ pages/overview.tsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from "react";
+
 import type { NextPage } from "next";
-import { useSelector } from "react-redux";
 import { RootState } from "../store";
+import { supabase } from "../supabase-client";
+import { useSelector } from "react-redux";
+
+type User = {
+	email: string;
+};
 
 const Overview: NextPage = () => {
 	const isAuth = useSelector<RootState, boolean>(
 		(state) => state.auth.isAuth
 	);
-	return <>{isAuth ? <div>logged</div> : <div>Please log in first</div>}</>;
+
+	const [users, setUsers] = useState<User[]>();
+
+	useEffect(() => {
+		const readUsers = async () => {
+			const { data } = await supabase.from("users").select("email");
+
+			setUsers(data as User[]);
+		};
+		readUsers().catch(() => {
+			throw new Error("Could not retrieve user data");
+		});
+	}, []);
+	return (
+		<>
+			{isAuth ? (
+				<div>{users && users[0].email}</div>
+			) : (
+				<div>Please log in first</div>
+			)}
+		</>
+	);
 };
 
 export default Overview;
